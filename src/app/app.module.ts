57b2a9270d4e169e59c1df2a5c2713e629bc81d6
Said Maadan <me@saidmaadan.com>
2017-05-05 16:34:32 -0500

    Installed required plugins and created needed providers and components

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+import { IonicStorageModule } from '@ionic/storage';
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { Keyboard } from '@ionic-native/keyboard';
+
+import { Reddit } from '../providers/reddit';
+import { Data } from '../providers/data';
+import { MyApp } from './app.component';
+// import { HomePage } from '../pages/home/home';
+
+@NgModule({
+  declarations: [
+    MyApp
+  ],
+  imports: [
+    BrowserModule,
+    HttpModule,
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot()
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp
+  ],
+  providers: [
+    Data,
+    Reddit,
+    StatusBar,
+    SplashScreen,
+    InAppBrowser,
+    Keyboard,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
